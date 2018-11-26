3b6a26284b4195fbcda50d3268c590ce154c3f2c
Sven Strittmatter <sven.strittmatter@iteratec.de>
2018-11-26 11:38:53 +0100

    Add build version in script

--- img-debian-nodejs-v8/release.sh
+++ img-debian-nodejs-v8/release.sh
@@ -2,9 +2,11 @@
 
 set -eu
 
+BUILD_VERSION="1.0.0"
+
 docker image build \
     --no-cache=true \
     --build-arg \
     BUILD_DATE="$(date -u +'%Y-%m-%dT%H:%M:%SZ')" \
-    -t weltraumschaf/debian-nodejs-v8:1.0.0 . && \
-docker push weltraumschaf/debian-nodejs-v8:1.0.0
+    -t weltraumschaf/debian-nodejs-v8:$BUILD_VERSION . && \
+docker push weltraumschaf/debian-nodejs-v8:$BUILD_VERSION
--- img-java8-base/release.sh
+++ img-java8-base/release.sh
@@ -2,9 +2,11 @@
 
 set -eu
 
+BUILD_VERSION="1.0.0"
+
 docker image build \
     --no-cache=true \
     --build-arg \
     BUILD_DATE="$(date -u +'%Y-%m-%dT%H:%M:%SZ')" \
-    -t weltraumschaf/java8-base:1.0.0 . && \
-docker push weltraumschaf/java8-base:1.0.0
+    -t weltraumschaf/java8-base:$BUILD_VERSION . && \
+docker push weltraumschaf/java8-base:$BUILD_VERSION
