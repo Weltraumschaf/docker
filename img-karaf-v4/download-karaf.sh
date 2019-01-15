bffeb53e6999c3acc575ec77fe7317ff619e2514
Sven Strittmatter <sven.strittmatter@iteratec.de>
2019-01-15 22:34:36 +0100

    Fix karaf image

--- img-karaf-v4/download-karaf.sh
+++ img-karaf-v4/download-karaf.sh
@@ -4,7 +4,7 @@ set -ue
 
 cwd=$(pwd)
 
-karaf_version="4.2.1"
+karaf_version="4.2.2"
 karaf_base_url="$(curl -s https://www.apache.org/dyn/closer.cgi\?preferred\=true)karaf/${karaf_version}/"
 karaf_dist="apache-karaf-${karaf_version}"
 karaf_dist_file_name="${karaf_dist}.tar.gz"
@@ -13,4 +13,5 @@ karaf_distribution="${cwd}/${karaf_dist_file_name}"
 
 echo "Download ${karaf_download_url} ..."
 curl -s "${karaf_download_url}" --output "${karaf_distribution}"
+echo "Unarchive ${karaf_distribution} ..."
 tar xzvf "${karaf_dist_file_name}" && rm -rfv "${karaf_dist_file_name}"
