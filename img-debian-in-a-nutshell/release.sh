4622f5e405862421ff71bf9dec77de66fd640241
Sven Strittmatter <ich@weltraumschaf.de>
2019-12-19 22:06:00 +0100

    Make no-cache optional
    Signed-off-by: Sven Strittmatter <ich@weltraumschaf.de>

--- img-debian-in-a-nutshell/release.sh
+++ img-debian-in-a-nutshell/release.sh
@@ -3,9 +3,10 @@
 set -eu
 
 BUILD_VERSION="1.0.0"
+NO_CACHE=${1:-"false"}
 
 docker image build \
-    --no-cache=true \
+    --no-cache=${NO_CACHE} \
     --build-arg BUILD_DATE="$(date -u +'%Y-%m-%dT%H:%M:%SZ')" \
     --build-arg BUILD_VERSION="${BUILD_VERSION}" \
     --build-arg VCS_REF="$(git rev-parse --short HEAD)" \
--- img-debian-nodejs-v8/release.sh
+++ img-debian-nodejs-v8/release.sh
@@ -3,8 +3,10 @@
 set -eu
 
 BUILD_VERSION="1.0.0"
+NO_CACHE=${1:-"false"}
 
 docker image build \
+    --no-cache=${NO_CACHE} \
     --no-cache=true \
     --build-arg BUILD_DATE="$(date -u +'%Y-%m-%dT%H:%M:%SZ')" \
     --build-arg BUILD_VERSION="${BUILD_VERSION}" \
