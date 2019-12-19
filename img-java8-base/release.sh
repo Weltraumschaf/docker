6b7f21ef9ef799edd815c596c267420f7abaf046
Sven Strittmatter <ich@weltraumschaf.de>
2019-12-19 21:35:01 +0100

    Fix the labels
    Signed-off-by: Sven Strittmatter <ich@weltraumschaf.de>

--- img-java8-base/release.sh
+++ img-java8-base/release.sh
@@ -6,7 +6,8 @@ BUILD_VERSION="1.0.0"
 
 docker image build \
     --no-cache=true \
-    --build-arg \
-    BUILD_DATE="$(date -u +'%Y-%m-%dT%H:%M:%SZ')" \
+    --build-arg BUILD_DATE="$(date -u +'%Y-%m-%dT%H:%M:%SZ')" \
+    --build-arg BUILD_VERSION="${BUILD_VERSION}" \
+    --build-arg VCS_REF="$(git rev-parse --short HEAD)" \
     -t weltraumschaf/java8-base:$BUILD_VERSION . && \
 docker push weltraumschaf/java8-base:$BUILD_VERSION
