7eacc8bbbd6e20ef7ce999bfcbd94f9ddd304e4e
Sven Strittmatter <ich@weltraumschaf.de>
2020-01-18 14:07:15 +0100

    Own Kali image
    Signed-off-by: Sven Strittmatter <ich@weltraumschaf.de>

---
+++ img-kali/release.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+set -eu
+
+BUILD_VERSION="1.0.0"
+NO_CACHE=${1:-"false"}
+
+docker image build \
+    --no-cache=${NO_CACHE} \
+    --build-arg BUILD_DATE="$(date -u +'%Y-%m-%dT%H:%M:%SZ')" \
+    --build-arg BUILD_VERSION="${BUILD_VERSION}" \
+    --build-arg VCS_REF="$(git rev-parse --short HEAD)" \
+    -t weltraumschaf/kali:$BUILD_VERSION . && \
+docker push weltraumschaf/kali:$BUILD_VERSION
