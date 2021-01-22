374d7f1cef516814dd2258c7cde53b2498083e8d
Sven Strittmatter <ich@weltraumschaf.de>
2021-01-22 17:25:08 +0100

    Fix karaf download and always use latest
    Signed-off-by: Sven Strittmatter <ich@weltraumschaf.de>

--- img-karaf-v4/download-karaf.sh
+++ img-karaf-v4/download-karaf.sh
@@ -4,11 +4,12 @@ set -ue
 
 cwd=$(pwd)
 
-karaf_version="4.2.8"
-karaf_base_url="$(curl -s https://www.apache.org/dyn/closer.cgi\?preferred\=true)karaf/${karaf_version}/"
+karaf_version="latest"
+karaf_mirror="$(curl -s https://www.apache.org/dyn/closer.cgi\?preferred\=true)"
+karaf_base_url="${karaf_mirror}/karaf"
 karaf_dist="apache-karaf-${karaf_version}"
 karaf_dist_file_name="${karaf_dist}.tar.gz"
-karaf_download_url="${karaf_base_url}${karaf_dist_file_name}"
+karaf_download_url="${karaf_base_url}/${karaf_dist_file_name}"
 karaf_distribution="${cwd}/${karaf_dist_file_name}"
 
 echo "Download ${karaf_download_url} ..."
